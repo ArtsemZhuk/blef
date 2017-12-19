5d563b452478a5ab29afcaa72d01350e51f9ba33
Artsem Zhuk <zhuk1996@gmail.com>
2017-12-19 04:13:55 +0300

    state

--- telegram-bot/logic.py
+++ telegram-bot/logic.py
@@ -1,7 +1,6 @@
-from random import shuffle, seed
+from random import shuffle, seed, randint
 from hashlib import sha256
-
-# shift = int(sha256(l1.encode("utf8")).hexdigest(), 16)
+from copy import copy
 
 def get_cards():
     cards = []
@@ -11,38 +10,33 @@ def get_cards():
     return cards
 
 
-def get_hand(me, ratio, hsh):
-    cards = get_cards()
-    seed(hsh + sum(ratio))
-    shuffle(cards)
+class State:
 
-    deck = []
-    my = []
-    for i in range(len(ratio)):
-        for t in range(ratio[i]):
-            x, cards = cards[0], cards[1:]
-            deck += [x]
-            if i == me:
-                my += [x]
-    return sorted(my)
+    def __init__(self, ratio):
+        self.hsh = randint(0, 2 ** 32 - 1)
+        self.ratio = ratio
 
+    def get_deck(self):
+        seed(self.hsh)
+        deck = get_cards()
+        shuffle(deck)
+        return deck
 
-def get_all(ratio, hsh):
-    cards = get_cards()
-    seed(hsh + sum(ratio))
-    shuffle(cards)
+    def get_hand(self, me):
+        l = sum(ratio[:me])
+        r = sum(ratio[:me + 1])
+        deck = self.get_deck()
+        return sorted(deck[l:r])
 
-    deck = []
-    for i in range(len(ratio)):
-        for t in range(ratio[i]):
-            x, cards = cards[0], cards[1:]
-            deck += [x]
-    return sorted(deck)
-
+    def get_table(self):
+        deck = self.get_deck()
+        table = deck[:sum(self.ratio)]
+        return table
+        
 
 if __name__ == '__main__':
     ratio = [1, 2, 3]
-    hsh = 123
+    s = State(ratio)
     for me in range(0, 3):
-        print(get_hand(me, ratio, hsh))
-    print(get_all(ratio, hsh))
+        print(s.get_hand(me))
+    print(s.get_table())
